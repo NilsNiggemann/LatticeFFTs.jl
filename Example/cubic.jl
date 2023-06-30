6495857c2146c35489142af97ce0590f03568c4d
Nils <nils.niggemann@fu-berlin.de>
2023-06-30 09:16:14 +0200

    wip

--- Example/cubic.jl
+++ Example/cubic.jl
@@ -8,7 +8,7 @@ function naiveDFT(chiR)
     for k in 0:N-1
         chik = 0. +0im
         for n in 0:N-1
-            chik += chiR[n+1]*exp(-1im*2pi*k*n/N)
+            chik += chiR[n+1]*exp(-1im*2pi*k*n/N)*exp(1im*pi*(k+1))
             # chik += chiR[j]*cos(2pi*k'*n/N)
         end
         chiK[k+1] = real(chik)
@@ -18,39 +18,49 @@ function naiveDFT(chiR)
 end
 function naiveFT(k,chiR)
     chik = 0.
-    for n in 0:length(chiR)-1
-        chik += chiR[n+1]*cos(k*n)
+    N = length(chiR)-1
+    for n in eachindex(chiR)
+        chik +=(-1)^n*exp(-n^2/20)*cos(k*n)
+        # @info "test" n chiR[abs(n)+1] (-1)^n*exp(-n^2/20)
+        # chik += chiR[n]*cos(k*n)
     end
     return chik
 end
+function dressFT!(chiK)    
+    for k in CartesianIndices(chiK)
+        # chiK[k] *= exp(1im*pi*sum(Tuple(k)))
+        isodd(sum(Tuple(k))) && (chiK[k] *=-1)
+    end
+    return chiK
+end
 ##
 let 
-    f1= 15.5
-    f2 = 100
-    N = 500
-    # chi = OffsetArrays.centered(zeros(N))
-    chi = zeros(N)
+    f1= 0.5*1pi
+    N = 401
+    chi = OffsetArrays.centered(zeros(N))
+    # chi = zeros(N)
     for i in eachindex(chi)
-        chi[i] = (-1)^(i)*exp(-abs(i)/5)
+        chi[i] = (-1)^(i)*exp(-(i)^2/20)
+        # chi[i] = cos(f1*i)*exp(-(i)^2/40)
         # chi[i] = 1
     end
-    # chi = OffsetArrays.no_offset_view(chi)
-
-    # lines(collect(eachindex(chi)),chi) |> display
-    
-    chiKfft = real(fft(chi))
-    chiK = real(naiveDFT(chi))
-    k2 = LinRange(-pi,pi,1000)
+    k2 = LinRange(-pi,pi,100)
     chiKCont = [naiveFT(k,chi) for k in k2]
+    chi = OffsetArrays.no_offset_view(chi)[1:end-1]
+    N-=1
+    lines(collect(eachindex(chi)),chi) |> display
+    chiKfft = real(dressFT!(fft(chi)))
+    chiK = real(naiveDFT(chi))
     # chiK = naiveDFT(chi)
     k = fftfreq(N)*2
     # push!(chiK,chiK[1])
     # k = 2piN*eachindex(chiK))
     scatter(k,chiK)
-    scatter!(k,chiKfft)
+    # scatter!(k,chiKfft)
     lines!(k2./pi,chiKCont)
+    hlines!([8])
     # lines(k,OffsetArrays.no_offset_view(chiK))
-    # vlines!([f1,f2],color = :red)
+    vlines!([f1/pi],color = :red)
     # xlims!(0,2*f2)
     current_figure()
 end
@@ -66,30 +76,30 @@ end
 ##
 
 let 
-    N = 200
+    N = 101
     chiR = OffsetArrays.centered(zeros(N,N))
     # chiR = zeros(N,N)
-    order = SA[1.5,1.5]*2pi
+    order = 0.2SA[1,1]*pi
     for ij in CartesianIndices(chiR)
         k = SVector(Tuple(ij))
         # cij = CubicAFMCorr(k)
-        cij = Cubicspiral(k,order/N,N/2)
+        cij = Cubicspiral(k,order,N)
         chiR[ij] = cij
     end
+    chiR = OffsetArrays.no_offset_view(chiR)
+
     # chiR = OffsetArrays.no_offset_view(chiR)
-    # heatmap(collect(axes(chiR,1)),collect(axes(chiR,2)),OffsetArrays.no_offset_view(chiR),axis = (;aspect=1)) |> display
-    # chiK = ifft(chiR)*N
-    chiK = naiveDFT(chiR)
-    @info "chiK" maximum(imag(chiK))
+    heatmap(collect(axes(chiR,1)),collect(axes(chiR,2)),chiR[:,:,1],axis = (;aspect=1)) |> display
+    chiK = fftshift(dressFT!(fft(chiR)))
+    # chiK = naiveDFT(chiR)
+    # @info "chiK" maximum(imag(chiK))
     chiK = real(chiK)
-    k = fftshift(fftfreq(N) )*N*2
+    k = fftshift(fftfreq(N))*2
     fig = Figure()
     ax = Axis(fig[1,1],aspect = 1)
-    hm = heatmap!(ax,OffsetArrays.no_offset_view(chiK))
+    hm = heatmap!(ax,k,k,chiK[:,:,1])
     # hm = heatmap!(ax,k,k,fftshift(chiK))
-    scatter!(ax,Point2(order...)/pi)
-    lims = 100pi
-    limits!(ax,-lims,lims,-lims,lims)
+    scatter!(ax,Point2(order[1:2]...)/pi,markersize = 20,marker =  '×',color =:red)
     Colorbar(fig[1,2],hm)
     fig
 end
