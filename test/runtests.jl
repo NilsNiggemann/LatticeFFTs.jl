4eaeb89e768ca69b1e8259c92740eb246d2ebed5
Nils <nils.niggemann@fu-berlin.de>
2023-07-13 16:35:00 +0200

    test Cubic

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,15 @@
-using Test, LatticeFFTs,OffsetArrays
+using Test,OffsetArrays, StaticArrays
 ##
-function naiveFT(k,chiR,func)
+function naiveFT(k::AbstractVector,chiR,func)
     chik = 0. +0im
-    for n in eachindex(chiR)
-        chik +=func(n)*exp(1im*k*n)
+    for n in CartesianIndices(chiR)
+        nVec = SA[Tuple(n)...]
+        chik +=func(nVec...)*exp(1im*k'*nVec)
     end
     return chik
 end
 
-@testset "interpolated FFT" begin
+@testset "1D chain" begin
     f1= 0.8*1pi
     for shift in (0,1)
         N = 3002+1
@@ -24,7 +25,38 @@ end
         chikextr = LatticeFFTs.getInterpolatedFFT(chiArray,512)
 
         for k in (f1,pi,0,0.13)
-            @test real(naiveFT(k,chi,func2)) ≈ real(chikextr(k)) atol = 1e-4
+            @test real(naiveFT(SA[k],chi,func2)) ≈ real(chikextr(k)) atol = 1e-4
         end
     end
+end
+
+function Cubicspiral(n,k,xi=10000)
+    return (cos(k'*n)) *exp(-n'*n/xi)
+end
+##
+@testset "Cubic" begin
+    N = 40 +1
+    chiR = OffsetArrays.centered(zeros(N,N,N))
+    # chiR = zeros(N,N)
+    order = 0.8SA[1,1,0]*pi
+    Config(n) = Cubicspiral(SA[Tuple(n)...],order,1)
+    Config(n1,n2,n3) = Cubicspiral(SA[n1,n2,n3],order,1)
+     
+    for ij in CartesianIndices(chiR)
+        k = SVector(Tuple(ij))
+        # cij = CubicAFMCorr(k)
+        cij = Config(ij)
+        chiR[ij] = cij
+    end
+    chiRArray = OffsetArrays.no_offset_view(chiR)
+    
+    @time chik = LatticeFFTs.getInterpolatedFFT(chiRArray,128)
+
+    chikNaive(k) = real(naiveFT(k,chiR,Config))
+    chikFFT(k) = real(chik(k...))
+
+    @test chikNaive(order) ≈ chikFFT(order) atol = 1e-3
+    @test chikNaive(SA[pi,pi,pi]) ≈ chikFFT(SA[pi,pi,pi]) atol = 1e-4
+    @test chikNaive(SA[0,0,0]) ≈ chikFFT(SA[0,0,0]) atol = 1e-4
+    @test chikNaive(SA[0.13,0.5,√2]) ≈ chikFFT(SA[0.13,0.5,√2]) atol = 1e-3
 end
